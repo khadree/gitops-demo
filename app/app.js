40d15544bca9db578cb2005023e3533a510507e2
khadreeg <khadreegeorge@gmail.com>
2026-05-22 18:34:21 +0100

    add

--- app/app.js
+++ app/app.js
@@ -1,17 +1,17 @@
 const http = require('http');
+const os = require('os');
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.end(`
-    <h1>🚀 GitOps Demo App</h1>
+    <h1>GitOps Demo App</h1>
     <p>Version: 1.0.0</p>
-    <p>Hostname: ${require('os').hostname()}</p>
+    <p>Hostname: ${os.hostname()}</p>
     <p>Deployed via ArgoCD + Kind!</p>
   `);
 });
 
 server.listen(port, () => {
-  console.log(\`App running on port \${port}\`);
+  console.log(`App running on port ${port}`);
 });
-    
